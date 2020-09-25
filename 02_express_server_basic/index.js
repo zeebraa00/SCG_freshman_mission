656e5f2cbf9b61080cd9617e0612263ca9133e96
zeebraa00 <zeebraa00@gmail.com>
2020-09-25 23:50:10 +0900

    [add] express를 이용해서 가장 기본적인 서버 구성하기

---
+++ 02_express_server_basic/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app =  express();
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/' + 'index.html');
+    // __dirname : 현재폴더의 절대경로 반환
+});
+
+app.listen(80, () => console.log('Server listen on port 80'));
