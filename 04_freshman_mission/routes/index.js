c73c7797622e7b67cfc86f31afde9e9c9ef6dace
zeebraa00 <zeebraa00@gmail.com>
2020-09-27 00:10:05 +0900

    [edit] url routing, express static 사용

--- 04_freshman_mission/routes/index.js
+++ 04_freshman_mission/routes/index.js
@@ -1,27 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-// router.get('/', function(req, res) {
-//   res.send({
-//     query:req.query,
-//     param:req.params,
-//   })
-// });
-
 router.get('/', (reqs, res)=> {
   res.render("index");
 })
 
-router.get('/:id/:name', function(req, res) {
-  res.send({
-    query : req.query,
-    param:req.params,
-  })
-});
+router.get('/day1_1', (reqs, res)=> {
+  res.render("day1_1/index");
+})
 
-router.post('/post', function(req, res) {
-  res.send(req.body);
-});
+router.get('/day1_2', (reqs, res)=> {
+  res.render("day1_2/index");
+})
+
+router.get('/day1_3', (reqs, res)=> {
+  res.render("day1_3/index");
+})
+
+router.get('/day2_1', (reqs, res)=> {
+  res.render("day2_1/index");
+})
+
+router.get('/day2_2', (reqs, res)=> {
+  res.render("day2_2/index");
+})
+
+router.get('/day3', (reqs, res)=> {
+  res.render("day3/index");
+})
 
 module.exports = router;
